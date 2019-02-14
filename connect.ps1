f5400f96f151027bca5ce6887bd9ef844d98ddd6
Timothy <timothy.makram.ghatas@be.ey.com>
2019-02-14 11:06:20 +0100

    added backup functionnality

--- connect.ps1
+++ connect.ps1
@@ -1,1 +1,21 @@
+
+function ZipFiles( $zipfilename, $sourcedir )
+{
+   Add-Type -Assembly System.IO.Compression.FileSystem
+   $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+   [System.IO.Compression.ZipFile]::CreateFromDirectory($sourcedir,
+        $zipfilename, $compressionLevel, $false)
+}
+
+function start-backup
+{
+$sourceDirectory  = "C:\Source\*"
+$destinationDirectory = "C:\Destination\"
+
+Copy-item -Force -Recurse -Verbose $sourceDirectory -Destination $destinationDirectory
+ZipFiles $backup.zip $destinationDirectory
+
+}
+
+
 Invoke-Command -HostName Ubuntu1 -UserName hackerman -ScriptBlock {start-backup}
